e9f8fcaffcae4390769922431b667608961fb09e
jaderoliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-03-04 22:28:28 -0300

    Update Aluno.php
    Criado um protected $guard para o atributo Id liberando assim todos os outros atributos para modificações em massa...

--- app/Aluno.php
+++ app/Aluno.php
@@ -12,10 +12,10 @@ class Aluno extends Model
      * @var string
      */
     protected $table = 'aluno';
+
     /**
-     * The list of models classes that should not be affected with touch.
-     *
-     * @var array
-     */
-    protected $fillable = Array('nome', 'email', 'sexo', 'endereco', 'telefone');
+    * The attributes that aren't mass assignable. 
+    * @var array
+    */
+    protected $guarded = ['id']; 
 }
