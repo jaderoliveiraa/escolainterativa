464a4245ec511bff50a0ed08a586492b7d8cda17
jaderoliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-03-04 22:30:42 -0300

    Update Aluno.php
    Já como teste foi adicionado no banco de dados os alunos:
    
     App\Aluno {#3035
             id: 1,
             nome: "João Silva",
             email: "joao@joao.com",
             sexo: "MASCULINO",
             endereco: "rua n",
             telefone: "88988424488",
             created_at: "2020-03-05 01:24:42",
             updated_at: "2020-03-05 01:24:42",
           },
           App\Aluno {#3037
             id: 2,
             nome: "Jader Oliveira",
             email: "jaderoliveiraa@gmail.com",
             sexo: "MASCULINO",
             endereco: "rua vereador xavier de oliveira, 68",
             telefone: "88988420622",
             created_at: "2020-03-05 01:26:03",
             updated_at: "2020-03-05 01:26:03",

--- app/Aluno.php
+++ app/Aluno.php
@@ -18,4 +18,25 @@ class Aluno extends Model
     * @var array
     */
     protected $guarded = ['id']; 
+
+    /**
+    * App\Aluno {#3035
+         id: 1,
+         nome: "João Silva",
+         email: "joao@joao.com",
+         sexo: "MASCULINO",
+         endereco: "rua n",
+         telefone: "88988424488",
+         created_at: "2020-03-05 01:24:42",
+         updated_at: "2020-03-05 01:24:42",
+       },
+       App\Aluno {#3037
+         id: 2,
+         nome: "Jader Oliveira",
+         email: "jaderoliveiraa@gmail.com",
+         sexo: "MASCULINO",
+         endereco: "rua vereador xavier de oliveira, 68",
+         telefone: "88988420622",
+         created_at: "2020-03-05 01:26:03",
+         updated_at: "2020-03-05 01:26:03",*/
 }
