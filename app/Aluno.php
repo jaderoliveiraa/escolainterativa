38ab07b0f05d803e208987e343224c10523299ef
jaderoliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-03-04 21:56:30 -0300

    Update Aluno.php
    Inserindo Dados no Banco de dados

--- app/Aluno.php
+++ app/Aluno.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aluno extends Model
 {
-    //
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'aluno';
+    /**
+     * The list of models classes that should not be affected with touch.
+     *
+     * @var array
+     */
+    protected $fillable = Array('nome', 'email', 'sexo', 'endereco', 'telefone');
 }
