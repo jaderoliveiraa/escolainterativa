f1d1ae31c6a430cce2b3d4bd4e28f73e73d88093
jaderoliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-03-05 22:19:47 -0300

    Update AlunoRepository.php
    Criando método construtor para Model Aluno

---
+++ app/GestaoEscolar/Aluno/AlunoRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\GestaoEscolar\Aluno;
+
+class AlunoRepository
+{
+    private aluno;
+    public function __construct(Aluno $aluno){
+
+        this => aluno = $aluno;
+
+    }
+
+}
