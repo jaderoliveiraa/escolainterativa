787e921bd9bbd41c8867fbd2963d2354b6bcfb16
jaderoliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-03-05 22:38:51 -0300

    Update AlunoService.php
    Aplicando comentários em AlunoService

--- app/GestaoEscolar/Aluno/AlunoService.php
+++ app/GestaoEscolar/Aluno/AlunoService.php
@@ -6,7 +6,18 @@ use AlunoRepository;
 
 class AlunoService
 {
+	/*
+	*Instancia de AlunoRepository
+	* @var AlunoRepository
+	*/
     private alunoRepository;
+
+    /*
+	*Instancia de Classe AlunoService
+	* @param App\GestaoEscolar\Aluno\AlunoRepository
+	* @param AlunoRepository
+	*/
+
     public function __construct(AlunoRepository $alunoRepository){
 
         this => alunoRepository = $alunoRepository;
