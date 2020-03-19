e22099a054d268b4dbf843c5e91508a298b7b15e
Jáder Oliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-03-19 00:09:55 -0300

    Create menuPrincipal.php
    Criação do arquivo menuPrincipal dentro da pasta views fora do framework

---
+++ views/menuPrincipal.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <script src="../system/js/bootstrap.js" type="text/javascript"></script>
+        <link href="../system/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Relatórios</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Cadastro
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Alunos</a>
+                            <a class="dropdown-item" href="#">Professores</a>
+                            <a class="dropdown-item" href="#">Cursos</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </body>
+</html>
