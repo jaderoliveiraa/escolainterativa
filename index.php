7410dfa011b591524963da9e4de7e126cecf2878
Jáder Oliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-03-19 00:08:09 -0300

    Create index.php
    Criação do arquivo index fora do framework

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <script src="system/js/bootstrap.js" type="text/javascript"></script>
+        <link href="system/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="system/css/estiloMenu.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="img/logo.png" id="icon" alt="User Icon" />
+    </div>
+
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="E-mail">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="Senha">
+      <input type="submit" class="fadeIn fourth" value="Entrar">
+    </form>
+
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+      <a class="underlineHover" href="#">Esqueçeu sua Senha?</a>
+    </div>
+
+  </div>
+</div>
+    </body>
+</html>
