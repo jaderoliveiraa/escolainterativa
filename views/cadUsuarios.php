da998c3ae00b8d6e2778cbab0e2d8be50e4fe60c
Jáder Oliveira <jaderoliveiraa@gmail.com>
2020-03-23 21:46:30 -0300

    Update cadUsuarios.php
    Concluido e testado o formulário de cadastro de usuarios

--- views/cadUsuarios.php
+++ views/cadUsuarios.php
@@ -14,49 +14,75 @@ include "menuPrincipal.php";
 </head>
 <body>
 
-    <div class="container">
-
-        <div class="central">
-
-
-                </p><br>
-                <form action="../controllers/controllerUsuarios.php" method="POST">
-                    <fieldset>
+    <form class="form-horizontal" action="../controllers/controllerUsuarios.php" method="POST">
+        <fieldset>
             <div class="panel panel-primary">
-                <div class="panel-heading">Cadastro de Alunos</div>
+                <div class="panel-heading">Cadastro de Usuários</div>
+
+
 
                 <div class="panel-body">
                     <div class="form-group">
-                    <div class="form-group">
-                        <label class="col-md-2 control-label" for="Nome">Nome <h11>*</h11></label>  
-                        <div class="col-md-8">
-                            <input class="form-control input-md" type="text" name="nome" placeholder="Digite o nome" required=""><br><br>
-                        </div>
-                    </div><br>
-                    
-                <div class="form-group">
-                    <label class="col-md-2 control-label" for="prependedtext">Telefone <h11>*</h11></label>
-                    <div class="col-md-2">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
-                    <input class="campo" id="tel" type="text" name="telefone" placeholder="XX XXXXX-XXXX" required="" type="text" maxlength="13" pattern="\[0-9]{2}\ [0-9]{4,6}-[0-9]{3,4}$"
-                                   OnKeyPress="formatar('## #####-####', this)">
+                        <div class="col-md-11 control-label">
+                            <p class="help-block"><h11>*</h11> Campo Obrigatório </p>
                         </div>
                     </div>
-                </div> 
-                    <label>E-mail</label><br>
-                    <input class="form-control input-md" type="text" name="email" id="email" placeholder="exemplo@exemplo.com.br"required="" maxlength="30"><br><br>
-                    <label>Senha</label><br>
-                    <input class="form-control input-md" type="text" name="senha" id="email" placeholder="Digite sua senha"required="" maxlength="30"><br><br>
-                    <button class="buttom" type="submit">Salvar</button>
-                </form>
-                </form>
-            </div>
-        </div>
-    </div>
-                        </div>
-            </div>
-        </div>
 
-</body>
-</html>
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-2 control-label" for="Nome">Nome <h11>*</h11></label>  
+                        <div class="col-md-7">
+                            <input id="Nome" name="nome" placeholder="Digite seu nome completo aqui" class="form-control input-md" required="" type="text">
+                        </div>
+                    </div>
+
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-2 control-label" for="prependedtext">Telefone <h11>*</h11></label>
+                        <div class="col-md-5">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+                                <input id="prependedtext" name="telefone" class="form-control" placeholder="XX XXXXX-XXXX" required="" type="text" maxlength="13" pattern="\[0-9]{2}\ [0-9]{4,6}-[0-9]{3,4}$"
+                                       OnKeyPress="formatar('## #####-####', this)">
+                            </div>
+                        </div>
+                    </div> 
+
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-2 control-label" for="prependedtext">E-mail<h11>*</h11></label>
+                        <div class="col-md-5">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+                                <input class="form-control input-md" type="text" name="email" id="email" placeholder="exemplo@exemplo.com.br"required="" maxlength="30">
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <!-- Text input-->
+                    <div class="form-group">
+                        <label class="col-md-2 control-label" for="prependedtext">Senha<h11>*</h11></label>
+                        <div class="col-md-5">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-asterisk"></i></span>
+                                <input class="form-control input-md" type="password" name="senha" id="email" placeholder="Digite sua senha"required="" maxlength="30"><br><br>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-2 control-label" for="Cadastrar"></label>
+                        <div class="col-md-8">
+                            <button id="Cadastrar" name="Cadastrar" class="btn btn-success" type="Submit">Cadastrar</button>
+                            <button id="Cancelar" name="Cancelar" class="btn btn-danger" type="Reset">Cancelar</button>
+                        </div>
+                    </div>                    
+                    </fieldtest>
+                    </form>
+                    </form>
+
+                </div>
+
+
+                </body>
+                </html>
