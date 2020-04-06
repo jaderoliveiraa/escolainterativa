b22154d9616d04ac2ca38b960a45385e78668762
Jáder Oliveira <jaderoliveiraa@gmail.com>
2020-04-05 22:38:56 -0300

    Update valida_login.php
    alterações na pagina de validação de login

--- controllers/valida_login.php
+++ controllers/valida_login.php
@@ -11,32 +11,33 @@
         require 'connect.php';
         require '../system/usuarioClass.php';
 
+
+
         if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['senha']) && !empty($_POST['senha'])) {
             $email = addslashes($_POST['email']);
             $senha = addslashes($_POST['senha']);
 
             $usuario = new usuarioClass();
-
-            if ($usuario->login($email, $senha) == true) {
-                if (isset($_SESSION['email'])) {
-                    echo "<script>alert('Bem vindo ao sistema Escola Interativa!);</script>";
-                    header("location:../views/inicial.php");
+            
+                if ($usuario->login($email, $senha) == true) {
+                    if (isset($_SESSION['email'])) {
+                        echo "<script>alert('Bem vindo ao sistema Escola Interativa!);</script>";
+                        header("location:../views/inicial.php");
+                    } else {
+                        echo '<script>alert("Email ou Senha incorretos!");</script>';
+                        header("location:../index.php");
+                    }
                 } else {
                     echo '<script>alert("Email ou Senha incorretos!");</script>';
                     header("location:../index.php");
-                    
                 }
             } else {
                 echo '<script>alert("Email ou Senha incorretos!");</script>';
                 header("location:../index.php");
-                
             }
-        } else {
-            echo '<script>alert("Email ou Senha incorretos!");</script>';
-            header("location:../index.php");
-            
-        }
         ?>
+
+
 
     </body>
 
