bfa94b92f73a82c20e9a6db67c44d39c7e62ce68
Jáder Oliveira <jaderoliveiraa@gmail.com>
2020-04-05 22:42:15 -0300

    Create rodape.php
    criação de um rodapé para o sistema. embreve faremos alterações no sistema visando preencher melhor essa seção!

---
+++ views/rodape.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="#"> Infotech</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
