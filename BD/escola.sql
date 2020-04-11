9905c09118c4ef1580afdf00a3c4af1d1988929c
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-11 16:07:22 -0300

    Update escola.sql
    Atualização do banco escola com algumas alterações na tabela usuarios (campo tipo)

--- BD/escola.sql
+++ BD/escola.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01-Abr-2020 às 23:09
+-- Tempo de geração: 10-Abr-2020 às 22:21
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.3
 
@@ -25,39 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `alunos`
---
-
-CREATE TABLE `alunos` (
-  `id` int(12) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `dataNasc` date NOT NULL,
-  `sexo` varchar(10) NOT NULL,
-  `telefone` varchar(30) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `senha` varchar(30) NOT NULL,
-  `cep` varchar(10) NOT NULL,
-  `rua` varchar(50) NOT NULL,
-  `num` int(10) NOT NULL,
-  `bairro` varchar(30) NOT NULL,
-  `cidade` varchar(50) NOT NULL,
-  `estado` varchar(5) NOT NULL,
-  `serie` varchar(25) NOT NULL,
-  `dataCadastro` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `idUsuarios` int(12) NOT NULL,
-  `situacao` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `alunos`
---
-
-INSERT INTO `alunos` (`id`, `nome`, `dataNasc`, `sexo`, `telefone`, `email`, `senha`, `cep`, `rua`, `num`, `bairro`, `cidade`, `estado`, `serie`, `dataCadastro`, `idUsuarios`, `situacao`) VALUES
-(1, 'menino teste', '2000-09-24', 'Masculino', '88 98841-5487', 'fulano@menino.com', '123456', '63034118', '25 de março', 321, 'Pirajá', 'Juazeiro', 'CE', 'Primeiro Ano', '2020-03-31 22:18:06', 14, 1);
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `usuarios`
 --
 
@@ -68,28 +35,25 @@ CREATE TABLE `usuarios` (
   `email` varchar(30) NOT NULL,
   `senha` varchar(30) NOT NULL,
   `dataCadastro` timestamp NOT NULL DEFAULT current_timestamp(),
-  `situacao` int(1) NOT NULL
+  `situacao` int(1) NOT NULL,
+  `tipo` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `telefone`, `email`, `senha`, `dataCadastro`, `situacao`) VALUES
-(1, 'Carlos jader da silva oliveira', '88988420622', 'jaderoliveiraa@gmail.com', '21548721', '2020-03-23 03:00:00', 1),
-(14, 'admin', '0000000000000', 'admin@admin.com', '123456', '2020-03-30 19:43:24', 1),
-(15, 'asdasdasdaskjhkl', '88 88888-8888', 'kdjashdkajsd@lsakjdksaljd.com', '123456', '2020-03-31 22:58:00', 0);
+INSERT INTO `usuarios` (`id`, `nome`, `telefone`, `email`, `senha`, `dataCadastro`, `situacao`, `tipo`) VALUES
+(1, 'Carlos jader da silva oliveira', '88988420622', 'jaderoliveiraa@gmail.com', '21548721', '2020-03-23 03:00:00', 1, 1),
+(14, 'admin', '0000000000000', 'admin@admin.com', '123456', '2020-03-30 19:43:24', 1, 1),
+(15, 'Professor', '88 88888-8888', 'professor@professor.com', '123456', '2020-03-31 22:58:00', 1, 2),
+(16, 'responsavel', '88988765432', 'responsavel@responsavel.com', '123456', '2020-04-10 13:20:15', 1, 3),
+(17, 'aluno', '88988888888', 'aluno@aluno.com', '123456', '2020-04-10 13:22:44', 1, 4),
+(18, 'inativo', '88988888888', 'inativo@inativo.com', '123456', '2020-04-10 13:23:18', 0, 1);
 
 --
 -- Índices para tabelas despejadas
 --
-
---
--- Índices para tabela `alunos`
---
-ALTER TABLE `alunos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_usuarios` (`idUsuarios`);
 
 --
 -- Índices para tabela `usuarios`
@@ -102,26 +66,10 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de tabela `alunos`
---
-ALTER TABLE `alunos`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- Restrições para despejos de tabelas
---
-
---
--- Limitadores para a tabela `alunos`
---
-ALTER TABLE `alunos`
-  ADD CONSTRAINT `fk_usuarios` FOREIGN KEY (`idUsuarios`) REFERENCES `usuarios` (`id`);
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
