c134b294e33c79608c0d98d2a7f30e430e612e25
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-11 16:12:42 -0300

    Update valida_login.php
    incluido variavel tipo e swicth case para direcionamento de pagina conforme nivel do usuario.

--- controllers/valida_login.php
+++ controllers/valida_login.php
@@ -8,6 +8,7 @@
     <body>
 
         <?php
+        global $menu;
         require 'connect.php';
         require '../system/usuarioClass.php';
 
@@ -16,25 +17,41 @@
         if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['senha']) && !empty($_POST['senha'])) {
             $email = addslashes($_POST['email']);
             $senha = addslashes($_POST['senha']);
+            $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+            $sql->bindValue(":email", $_POST['email']);
+            $sql->execute();
+            $res = $sql->fetch();
 
             $usuario = new usuarioClass();
-            
-                if ($usuario->login($email, $senha) == true) {
-                    if (isset($_SESSION['email'])) {
-                        echo "<script>alert('Bem vindo ao sistema Escola Interativa!);</script>";
-                        header("location:../views/inicial.php");
-                    } else {
-                        echo '<script>alert("Email ou Senha incorretos!");</script>';
-                        header("location:../index.php");
+
+            if ($usuario->login($email, $senha) == true) {
+                if (isset($_SESSION['email'])) {
+                    switch ($res['tipo']) {
+                        case 1:
+                            header("location:../views/inicial.php");
+                            break;
+                        case 2:
+                            header("location:../views/inicialProfessor.php");
+                            break;
+                        case 3:
+                            header("location:../views/inicialResponsavel.php");
+                            break;
+                        case 4:
+                            header("location:../views/inicialAluno.php");
+                            break;
+
+                        default:
+                            break;
                     }
                 } else {
-                    echo '<script>alert("Email ou Senha incorretos!");</script>';
-                    header("location:../index.php");
+                    echo "<script> alert('Usuário ou senha inválidos!'); window.location.href = '../index.php';</script>";
                 }
             } else {
-                echo '<script>alert("Email ou Senha incorretos!");</script>';
-                header("location:../index.php");
+                echo "<script> alert('Usuário ou senha inválidos!'); window.location.href = '../index.php';</script>";
             }
+        } else {
+            echo "<script> alert('Usuário ou senha inválidos!'); window.location.href = '../index.php';</script>";
+        }
         ?>
 
 
