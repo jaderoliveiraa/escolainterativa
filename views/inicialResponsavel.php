56c77682ccc4b84e7714168d44e4ffec137e9f2b
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-12 16:02:26 -0300

    Update inicialResponsavel.php
    atualização para include de um arquivo separado para tabela

--- views/inicialResponsavel.php
+++ views/inicialResponsavel.php
@@ -1,8 +1,8 @@
 <?php
 require '../controllers/connect.php';
 require './menuResponsavel.php';
-if (empty($_SESSION['email'])) {
-    header("location:../../index.php");
+if (empty($_SESSION['email'])&& empty($_SESSION['nome'])) {
+    header("location:../index.php");
 }
 ?>
 <html>
