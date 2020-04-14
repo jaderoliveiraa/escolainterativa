45b9f7ca677e9ba933f80e2574c13231d713554c
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-13 23:26:44 -0300

    Create divs.js
    Criação de JS para tratamento de novas divs condicionais no cadastro de usuario unificado

---
+++ system/js/divs.js
@@ -0,0 +1,25 @@
+//Funções após a leitura do documento
+$(document).ready(function () {
+    ocultar();
+    //funcções exibir e ocultar
+    function exibir() {
+        document.getElementById("oculto").style.display = "block";
+        document.getElementById("oculto2").style.display = "block";
+    }
+
+    function ocultar() {
+        document.getElementById("oculto").style.display = "none";
+        document.getElementById("oculto2").style.display = "none";
+    }
+    //Select para mostrar e esconder divs
+    $('#tipo').on('change', function () {
+        var selectValue = $(this).val();
+        if (selectValue !=="4") {
+            ocultar();
+            
+        }else{
+            exibir();
+        }
+
+    });
+});
