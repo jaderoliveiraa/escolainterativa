0636ca2af8520d2dde4bedbbee6df14371bb039b
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-17 21:54:38 -0300

    Create adminRelatorios.php
    Criação da pagina de relatórios

---
+++ views/adminRelatorios.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php
+
+require_once './menuPrincipal.php';
+require_once '../controllers/connect.php';
+
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
