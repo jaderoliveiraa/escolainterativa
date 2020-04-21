68c9904ea40fa5018208402a124f35ee038add7b
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-21 16:21:23 -0300

    Create boletim.php
    Criação da view Boletim para impressão

---
+++ views/boletim.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <!--<link href="../system/css/form.css" rel="stylesheet" type="text/css"/>
+        <link href="../system/css/estiloMenu.css" rel="stylesheet" type="text/css"/>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>-->
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="../system/js/form.js" type="text/javascript"></script>
+        <script src="../system/js/divs.js" type="text/javascript"></script>
+        <meta charset="UTF-8">
+        <title>Boletim Escolar</title>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <table class="table table-striped table-hover table-sm" align="center" >
+                <thead class="titulo table-info text-success"><h5>Boletim escolar</h5> </br> 
+                <div class="row-inline">
+                    <div class="col-md-4">
+                        Aluno: 
+                        <input id="prependedtext" name="senha" class="form-control" placeholder="Aluno" required="" type="password">
+                    </div>
+                    <div class="col-md-4">
+                        Turno: 
+                        <input id="prependedtext" name="senha" class="form-control" placeholder="Turno" required="" type="password">
+                    </div>
+                    <div class="col-md-4">
+                        Ano: 
+                        <input id="prependedtext" name="senha" class="form-control" placeholder="Ano" required="" type="password">
+                    </div>
+                </div>
+
+                </thead>
+                <tr>
+                    <td align="center">Disciplina</td>
+                    <td align="center">1º Bimestre</td>
+                    <td align="center">2º Bimestre</td>
+                    <td align="center">3º Bimestre</td>
+                    <td align="center">4º Bimestre</td>
+                    <td align="center">Média Final</td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+                <tr>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                    <td><input></td>
+                </tr>
+
+
+            </table>
+
+            </br><a title='Imprimir conteúdo' href='javascript:window.print()' class="text-white"><button type="button" class="btn btn-primary" >
+                    <b>Imprimir</b>
+                </button></a>
+
+        </div>
+    </body>
+</html>
