e0c165a4bb82855585d1fc5b85172c1a996fae44
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-21 16:20:01 -0300

    Update controllerNotas.php
    Alteração no controller notas

--- controllers/controllerNotas.php
+++ controllers/controllerNotas.php
@@ -6,15 +6,23 @@ $periodo = $_POST['periodo'];
 $id_disciplina = $_POST['id_disciplina'];
 $id_aluno = $_POST['id_aluno'];
 
-echo $valor ."</br>";
-echo $tipo ."</br>";
-echo $periodo ."</br>";
-echo $id_disciplina ."</br>";
-echo $id_aluno ."</br>";
+//$sql = "INSERT INTO notas SET valor = '$valor' , tipo = '$tipo', periodo = '$periodo', id_disciplina = '$id_disciplina', id_aluno = '$id_aluno'";
+$sql = "INSERT INTO notas (valor, tipo, periodo, id_disciplina, id_aluno) VALUES(:valor, :tipo, :periodo, :id_disciplina, :id_aluno)";
+/* @var $pdo type */
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':valor', $valor);
+$stmt->bindParam(':tipo', $tipo);
+$stmt->bindParam(':periodo', $periodo);
+$stmt->bindParam(':id_disciplina', $id_disciplina);
+$stmt->bindParam(':id_aluno', $id_aluno);
 
-$sql = "INSERT INTO notas SET valor = '$valor' , tipo = '$tipo', periodo = '$periodo', id_disciplina = '$id_disciplina', id_aluno = '$id_aluno'";
-
-if($pdo->query($sql)){
+if ($stmt->execute()) {
+    echo "<script> alert('Nota Inserida com Sucesso!'); window.location.href = '../views/cadNotas.php';</script>";
+}else{
+    echo "Erro ao cadastrar";
+    print_r($stmt->errorInfo());
+}
+/*if($pdo->query($sql)){
     
     echo "<script> alert('Nota Inserida com Sucesso!'); window.location.href = '../views/cadNotas.php';</script>";
     //header("location:../formularios/formcadastro.php");
@@ -23,4 +31,4 @@ if($pdo->query($sql)){
 }else{
     echo "Não se pode inserir campos vazios!";
     echo "<script> alert('Dsiciplina não Inserida verifique e tende novamente!');</script>";
-}
+}*/
