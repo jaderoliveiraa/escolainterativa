71628c410b59df45d1c19b1984cfe35d1374bb68
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-21 16:23:14 -0300

    Create frequencia.php
    Criado o arquivo frequencia.php

---
+++ views/frequencia.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+include '../controllers/connect.php';
+require './menuPrincipal.php';
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+    
+    <?php
+    require_once './rodape.php';
+    ?>
+</html>
