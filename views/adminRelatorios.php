074797b1e761728069b50f225723ad05efa64b53
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-21 16:21:07 -0300

    Update adminRelatorios.php
    atualização de relatorios

--- views/adminRelatorios.php
+++ views/adminRelatorios.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <?php
-
 require_once './menuPrincipal.php';
 require_once '../controllers/connect.php';
-
 ?>
 <html>
     <head>
@@ -11,6 +9,45 @@ require_once '../controllers/connect.php';
         <title></title>
     </head>
     <body>
-        
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row"><br></div>
+                <div class="row">
+                    <div class="col-3">
+                        <a href="cadDisciplinas.php"> <div class="card-wrapper " style="width: 15rem;">
+                                <img class="card-img-top" src="../img/livros.jpg" alt="Imagem de capa do card">
+                                <div class="card-body">
+                                    <h2 class="card-title text-center">Disciplinas</h2>
+                                </div>
+                            </div></a>
+                    </div>
+                    <div class="col-3">
+                        <a href="#"><div class="card-wrapper" style="width: 15rem;">
+                                <img class="card-img-top" src="../img/BOLETIM.png" alt="Imagem de capa do card">
+                                <div class="card-body text-center">
+                                    <h2 class="card-title">Boletim Online</h2>
+                                </div>
+                            </div></a>
+                    </div>
+                    <div class="col-3">
+                        <a href="cadUsuarios.php"><div class="card-wrapper" style="width: 15rem;">
+                                <img class="card-img-top" src="../img/usuarios.png" alt="Imagem de capa do card">
+                                <div class="card-body">
+                                    <h2 class="card-title text-center">Usuários</h2>
+                                </div>
+                            </div></a>
+                    </div>
+                    <div class="col-3 mask flex-center">
+                        <a href="cadAlunos.php"><div class="card-wrapper" style="width: 15rem;">
+                                <img class="card-img-top" src="../img/alunos.png" alt="Imagem de capa do card">
+                                <div class="card-body">
+                                    <h2 class="card-title text-center " >Alunos</h2>
+                                </div>
+                            </div></a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </body>
 </html>
