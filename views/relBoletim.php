a6d67f62929862fbc4e5177216624d786a2701a3
Jáder Oliveiraa <jaderoliveiraa@gmail.com>
2020-04-21 16:23:42 -0300

    Create relBoletim.php
    criado a view Relatorio boletins

---
+++ views/relBoletim.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php
+include '../menuPrincipal.php';
+include_once '../../controllers/connect.php';
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
