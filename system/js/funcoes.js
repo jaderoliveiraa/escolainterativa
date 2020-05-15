d83d785b673af81c97a8b5eacf3affd2982574d3
Jáder Oliveiraa <jaderoliveriaa@gmail.com>
2020-05-14 23:43:05 -0300

    Create funcoes.js
    Criado o arquivo que guarda a função de inativar Usuários

---
+++ system/js/funcoes.js
@@ -0,0 +1,26 @@
+function mascCPF(texto){
+    if (event.keycode !== 8){
+        if (texto.length === 3 || texto.length === 7){
+           document.getElementById("cpf").value = texto + ".";
+        }else if(texto.length === 11){
+            document.getElementById("cpf").value = texto + "-";
+        }
+    }
+}
+function mascTEL(texto){
+    if(event.keycode !== 8){
+        if(texto.length === 2){
+            document.getElementById("tel").value = "(" + texto + ")";
+        }
+        if(texto.length === 9){
+            document.getElementById("tel").value = texto + "-";
+        }
+    }
+}
+function inativar(id){
+    var resposta = confirm("Deseja Inativar o Usuário?");
+    if(resposta === true){
+        alert('O Usuário agora está INATIVO!');
+        window.location.href = "../controllers/inativaUsuarios.php?id="+id;
+    }
+}
