6faae0b43780ce1f15228dba1fe43c6ba2593e49
Jáder Oliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-06-03 19:02:50 -0300

    Create teste.php

---
+++ views/teste.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link href="http://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css"/>
+        <script src="http://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+        <div>
+            $(document).ready( function () {
+            $('#myTable').DataTable();
+            } );
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
