80074bf03815793185165826a139c2e1f2b4612a
Jáder Oliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-06-03 23:31:02 -0300

    Update frequencia.php
    alteração do arquivo pra remoção da session

--- views/frequencia.php
+++ views/frequencia.php
@@ -6,6 +6,13 @@ require './menuPrincipal.php';
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <!--    <script src="js/jquery-3.3.1.js"></script>-->
+        <<script
+            src="https://code.jquery.com/jquery-3.5.1.js"
+            integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous"></script>
         <title>Lista de Frequência</title>
     </head>
     <body>
@@ -13,18 +20,18 @@ require './menuPrincipal.php';
             <!-- Tabela-->
             <div class="container-fluid">
                 <?php
-                $rs = $pdo->prepare("SELECT * FROM usuarios WHERE tipo = 4");
-                $rs->execute();
-                $disciplinas = $_POST['disciplinas'];
-                $id_serie = $_POST = ['id_serie'];
+                $disciplinas = $_GET['disciplinas'];
+                $id_serie = $_GET['id_serie'];
+                $rs = $pdo->prepare("SELECT * FROM usuarios WHERE tipo = 4 AND id_serie = $id_serie");
+                $rs->execute();;
                 ?>
 
                 <div class="container-fluid">
                     <div>
                         <Label>Disciplina</Label>
-                        <input id="disciplinas" name="cidade" class="form-control" placeholder=""  type="text" value="<?php echo $disciplinas ?>" readonly>
+                        <input id="disciplinas" name="disciplinas" class="form-control" placeholder=""  type="text" value="<?php echo $disciplinas ?>" readonly>
                         <label>Serie</label>
-                        <input id="serie" name="cidade" class="form-control" placeholder=""  type="text" value="<?php echo $id_serie ?>" readonly>
+                        <input id="serie" name="id_serie" class="form-control" placeholder=""  type="text" value="<?php echo $id_serie ?>" readonly>
                     </div>
                     <table class="table table-striped table-hover table-sm" align="center" >
                         <thead class="titulo table-info"><th colspan="9">Lista de Alunos</th></thead>
@@ -35,6 +42,7 @@ require './menuPrincipal.php';
                         <th class="">E-mail</th>
                         <th class="">Serie</th>
                         <th class="">Presença</th>
+                        <th class="">Status</th>
                         </thead>
                         <hr>
                         <tbody>
@@ -45,15 +53,9 @@ require './menuPrincipal.php';
                                     <td><?php echo $aluno['telefone']; ?></td>
                                     <td><?php echo $aluno['email']; ?></td>
                                     <td><?php echo $aluno['id_serie']; ?></td>
-                                    <td> 
-                                        <label required="" class="radio-inline" for="radios-0" >
-                                            <input name="presenca" id="presenca" value="p" type="radio">
-                                            Presente
-                                        </label>
-                                        <label class="radio-inline" for="radios-1">
-                                            <input name="presenca" id="presenca" value="a" type="radio">
-                                            Ausente
-                                        </label>
+                                    <td>
+                                    <button value="true" onclick="send(<?php echo $aluno['id']; ?>, this)" class="btn btn-success btn-sm">Presente</button>
+                <button value="false" onclick="send(<?php echo $aluno['id']; ?>, this)" class="btn btn-danger btn-sm">Faltou</button>
 
                                     </td>
                                 </tr>  
@@ -66,6 +68,28 @@ require './menuPrincipal.php';
 
             </div>
         </form>
+        <script>
+
+            function send(id, element) {
+                let form = new FormData();
+
+                form.append('id', id);
+                form.append('status', element.value);
+
+
+                fetch("../controllers/controllerFrequencia.php", {
+                    method:"GET",
+                    body: form
+                })
+                .then(response => response.json())
+                .then(response => {
+                    
+                })
+
+
+            }
+       
+    </script>
 
     </body>
 
