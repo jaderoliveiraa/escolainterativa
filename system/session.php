1ea24b1cdf95cc7bb12d9a51cc2cac1fb967a10e
Jáder Oliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2020-06-03 23:30:14 -0300

    Update session.php
    modificação do arquivo session

--- system/session.php
+++ system/session.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-if (empty($_SESSION['email']) && empty($_SESSION['nome'])) {
+/*if (empty($_SESSION['email']) && empty($_SESSION['nome'])) {
+    header("location:../index.php");
+}*/
+if (empty($_SESSION['email'])) {
     header("location:../index.php");
 }
 ?>
